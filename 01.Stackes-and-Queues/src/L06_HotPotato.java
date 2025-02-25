33a042a94a91f2c28c2348c8c44455416f66b118
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2025-02-25 17:32:38 +0200

    added L06_HotPotato

---
+++ 01.Stackes-and-Queues/src/L06_HotPotato.java
@@ -0,0 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Scanner;
+
+public class L06_HotPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        int toss = Integer.parseInt(scanner.nextLine());
+
+        ArrayDeque<String> children = new ArrayDeque<>(List.of(input.split("\\s+")));
+
+        while (children.size() > 1) {
+            for (int i = 0; i < toss - 1; i++) {
+                children.offer(children.poll());
+            }
+
+            System.out.printf("Removed %s%n", children.poll());
+        }
+
+        System.out.printf("Last is %s", children.poll());
+    }
+}

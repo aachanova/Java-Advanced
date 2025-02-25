adebcb0e5aa6041076bad3ce8f341790a692230b
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2025-02-25 23:49:07 +0200

    added L08_BrowserHistoryUpgrade

---
+++ 01.Stackes-and-Queues/src/L08_BrowserHistoryUpgrade.java
@@ -0,0 +1,32 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class L08_BrowserHistoryUpgrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> browserHistory = new ArrayDeque<>();
+        ArrayDeque<String> forwardHistory = new ArrayDeque<>();
+        String input = "";
+        while (!"Home".equals(input = scanner.nextLine())) {
+            if (!input.equals("back") && !input.equals("forward")) {
+                browserHistory.push(input);
+                System.out.println(browserHistory.peek());
+            } else if (input.equals("back")){
+                if (browserHistory.size() > 1) {
+                    forwardHistory.push(browserHistory.pop());
+                    System.out.println(browserHistory.peek());
+                } else {
+                    System.out.println("no previous URLs");
+                }
+            } else {
+                if (forwardHistory.size() > 0) {
+                    System.out.println(forwardHistory.peek());
+                    browserHistory.push(forwardHistory.pop());
+                } else {
+                    System.out.println("no next URLs");
+                }
+            }
+        }
+    }
+}

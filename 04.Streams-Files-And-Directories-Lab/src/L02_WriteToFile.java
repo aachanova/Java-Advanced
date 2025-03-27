3e7be9d098204d53e7806630c6af308508a8bf1f
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2025-03-27 23:52:57 +0200

    added L02_WriteToFile

---
+++ 04.Streams-Files-And-Directories-Lab/src/L02_WriteToFile.java
@@ -0,0 +1,25 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class L02_WriteToFile {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream inputStream = new FileInputStream("input.txt");
+        FileOutputStream outputStream = new FileOutputStream("output.txt");
+        PrintWriter writer = new PrintWriter(outputStream);
+        Set<Character> skipElements = new HashSet<>() {{ add('!'); add('?'); add(','); add('.');}};
+        int singleByte;
+        while ((singleByte = inputStream.read()) >= 0) {
+            char symbol = (char) singleByte;
+            if(!skipElements.contains(symbol)){
+                writer.print(symbol);
+            }
+        }
+        inputStream.close();
+        writer.close();
+    }
+}

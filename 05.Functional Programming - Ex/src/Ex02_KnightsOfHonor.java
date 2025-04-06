f7d07ac8270151c138bf188b818171cb4fe4d3b4
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2025-04-06 23:56:24 +0300

    added Ex02_KnightsOfHonor

--- 05.Functional Programming - Ex/src/Ex02_KnightsOfHonor.java
+++ 05.Functional Programming - Ex/src/Ex02_KnightsOfHonor.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 import java.util.function.Consumer;
 
 public class Ex02_KnightsOfHonor {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Consumer<String> print = s -> System.out.printf("Sir %s%n", s);
+        Arrays.stream(scanner.nextLine().split("\\s+")).forEach(print);
+    }
 }
 
